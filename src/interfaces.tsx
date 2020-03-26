3d813141ca4a600becc264c0e148b4f1661af4ce
emrejz <emre.kayacan@msn.com>
2020-03-27 02:19:42 +0300

    interfaces

---
+++ src/interfaces.tsx
@@ -0,0 +1,76 @@
+import { GameActionTypes } from "./pages/apps/game/store/actions";
+import { GlobalActionTypes } from "./store/actions";
+
+// GLOBAL-STATE
+export interface IGlobalContext {
+  state: IGlobalState;
+  dispatch: IGlobalDispatch;
+}
+export interface IGlobalState {
+  zIndexInit: number;
+  appList: IAppState[];
+}
+
+export interface IGlobalDispatch {
+  openApp: (payload: IGlobalActionPayload) => IGlobalState;
+  reSizeApp: (payload: IGlobalActionPayload) => IGlobalState;
+  minSizeApp: (payload: IGlobalActionPayload) => IGlobalState;
+}
+export interface IGlobalAction {
+  type: GlobalActionTypes;
+  payload: IGlobalActionPayload;
+}
+export interface IGlobalActionPayload {
+  name: string;
+  open?: boolean;
+}
+
+export interface IAppState {
+  name: string;
+  icon: string;
+  open: boolean;
+  size: string;
+  zIndex: number;
+  isActive: boolean;
+}
+
+//GAME-STATE
+export interface IGameContext {
+  gameState: IGameState;
+  gameDispatch: IGameDispatch;
+}
+export interface IGameState {
+  cellList: IGameCell[][];
+  gameStatus: string;
+  openedCell: number;
+  mineCounter: number;
+  flagCounter: number;
+}
+export interface IGameDispatch {
+  openCell: (payload: IGameCell) => IGameState;
+  flagCell: (payload: IGameCell) => IGameState;
+  startGame: () => IGameState;
+}
+
+export interface IGameAction {
+  type: GameActionTypes;
+  payload?: any;
+}
+
+export interface IGameCell {
+  flag: boolean;
+  mine: boolean;
+  open: boolean;
+  val: string | number;
+  x: number;
+  y: number;
+}
+//OTHER INTERFACES
+export interface ISelectedVscFile {
+  tag: string[];
+  title: string;
+  desc: string;
+  demo: string;
+  repo: string;
+  selected?: boolean;
+}
