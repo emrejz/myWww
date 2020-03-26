30199bf63ff357e8aca727053ab8272b25099232
emrejz <emre.kayacan@msn.com>
2020-03-27 02:24:01 +0300

    pages shortcut

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import BioApp from "./apps/bio";
+import GameApp from "./apps/game";
+import VscApp from "./apps/vsc";
+import Loading from "./loading";
+import Window from "./window";
+export { BioApp, GameApp, VscApp, Loading, Window };
