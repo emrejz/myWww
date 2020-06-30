e6fe7a913a30e4af24fb8ef8bb72f7642e9a7937
emrejz <emre.kayacan@msn.com>
2020-07-01 00:50:47 +0300

    updated bio

--- src/pages/apps/bio/comps/body/index.tsx
+++ src/pages/apps/bio/comps/body/index.tsx
@@ -20,24 +20,25 @@ const BioBody: React.FC<Props> = ({ state, dispatch }) => {
       className="bioBody"
       onClick={() => openApp({ name, open: true })}
     >
-      <Text mb="24px">name -> Emre</Text>
-      <Text mb="24px">surname -> Kayacan</Text>
-      <Text mb="24px">military service -> done</Text>
+      <Text mb="24px">name {"->"} Emre</Text>
+      <Text mb="24px">surname {"->"} Kayacan</Text>
+      <Text mb="24px">military service {"->"} done</Text>
       <Text mb="24px">
-        education -> Namık Kemal University Electronics and Communication
+        education {"->"} Namık Kemal University Electronics and Communication
         engr.2011-2018
       </Text>
-      <Text mb="24px">email -> emre.kayacan@msn.com</Text>
-      <Text mb="24px">git -> https://github.com/emrejz</Text>
+      <Text mb="24px">email {"->"} emre.kayacan@msn.com</Text>
+      <Text mb="24px">git {"->"} https://github.com/emrejz</Text>
       <Text mb="24px">
-        skills -> react, redux, vue, angular, NgRx, typeScript, next,
-        react-native, node, mongo, redis, graphql, socket, materialUI,
-        bootstrap, styled-components, pug, HTML/CSS/SCSS
+        skills {"->"} react, redux, hooks+context, vue, vuex, angular, NgRx,
+        typeScript, next, swr, react-native, webassembly, node, express, jwt &
+        bcrypt for auth, mongo, redis, graphql, socket, materialUI, bootstrap,
+        styled-components, pug, HTML/CSS/SCSS
       </Text>
       <Text mb="24px">
-        About me-> I graduated from university late, i had a health problem. I
-        worked at the factory for one year, but i wanted to quit my job to
-        advance my career in software.
+        About me {"->"} I graduated from university late, i had a health
+        problem. I worked at the factory for one year, but i wanted to quit my
+        job to advance my career in software.
         <br />I can use react,vue,angular,pure js in the front-end side.
         <br />I can use node in the back-end side.
         <br />I can use react-native in the mobile side. (I could develop an
@@ -46,14 +47,14 @@ const BioBody: React.FC<Props> = ({ state, dispatch }) => {
         design/task for test me.
       </Text>
       <Text mb="24px">
-        About this website-> designed by Bill Gates, developed by me. I used
-        react in the front-end. I used typescript, react hooks and context, scss
-        and styled - components in this website.
+        About this website {"->"} designed by Bill Gates, developed by me. I
+        used react in the front-end. I used typescript, react hooks and context,
+        scss and styled - components in this website.
         <br />
         Todo;
         <br />
         # winamp /papi papi papi çilo
-        <br /># batak /best turkish strategy game
+        <br /># batak /best turkish strategy game <a>&#x2714;</a>
         <br /># os notification sounds
       </Text>
     </Block>
